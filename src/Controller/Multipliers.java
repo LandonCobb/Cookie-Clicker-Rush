ae5fef12ef2f83914c42f8afda4257a96f9a6ebe
LandonCobb <landoncobb123@gmail.com>
2022-02-28 20:56:47 -0700

    added basic click class and added methods for multipliers

--- src/Controller/Multipliers.java
+++ src/Controller/Multipliers.java
@@ -1,5 +1,41 @@
 package Controller;
 
-public class Multipliers {
+import Model.Click;
 
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class Multipliers {
+    private int multValue;
+    Click click = new Click();
+
+    public int getMultValue() {
+        return multValue;
+    }
+
+    public void setMultValue(int multValue) {
+        this.multValue = multValue;
+    }
+
+    //basic multiplier
+    public void multClick(int multValue){
+        click.setClickValue(click.getClickValue() * multValue);
+    }
+
+    //timer multiplier/adder
+    public void bleedClick(){
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                //TODO cancel timer at certain point
+            }
+        }, 0, 2000);
+    }
+
+    public void randomClick(){
+        Random rnd = new Random();
+        click.setClickValue(rnd.nextInt(3) + 1);
+    }
 }
