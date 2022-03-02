6ccfe281830c6a594d7d6934498be3023006b363
bfinne05 <bfinne05@gmail.com>
2022-03-02 08:54:15 -0700

    I Completed the cookie demon code and changed some dialogue for the bosses we need to change the health values of the bosses later

--- src/Model/CookieDemon.java
+++ src/Model/CookieDemon.java
@@ -8,7 +8,39 @@ public class CookieDemon extends Character{
         super.setAttack(attack);
     }
     //////////////////////////////////////////////////
-
+    public void cookieDrainHealth(int cookie){
+        int hp;
+        if(cookie > 0){
+            hp = getHp() + cookie;
+            setHp(hp);
+            System.out.println("The Cookie Demon stole " + cookie + " cookies from you and ate them...how rude! Cookie Demon restored: " + cookie + " hp!");
+        }
+        else{
+            System.out.println("The Cookie Demon tries to snatch cookies from your bag but upon seeing your bag empty he laughs at how poor you are...you cry in sadness");
+        }
+    }
+/////////////////////////////////////////////////////////////////////////////
+    public int cookieDrainAttack(int cookie){
+        int cookieAttack;
+        if(cookie > 0){
+            cookieAttack = getAttack() + cookie;
+            System.out.println("The Demon magically took cookies out of your bag and tossed them at your face!!! you took: " + cookieAttack + " damage!");
+            return cookieAttack;
+        }
+        System.out.println("The Cookie Demon tries to snatch cookies from your bag but its empty. He sighs in disappointment, you felt mentally hurt by this, you took: " + getAttack() + " damage!");
+        return getAttack();
+    }
+    ////////////////////////////////////////////////
+    //need to change variables
+    @Override
+    public void setHp(int hp){
+        if(hp > 100){
+            this.hp = 100;
+        }
+        else{
+            this.hp = hp;
+        }
+    }
     ////////////////////////////////////////////////
     @Override
     public String toString() {
